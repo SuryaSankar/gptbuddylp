699b190e95dfddd0051c0b3a7dbf9a833377ec47
Surya Sankar <suryashankar.m@gmail.com>
2024-12-01 16:14:29 +0530

    Landing pages

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import nunjucks from 'vite-plugin-nunjucks';
+
+export default defineConfig({
+  plugins: [
+    nunjucks({
+        templatesDir: './',
+    }),
+  ],
+  root: 'src',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/index.html',       // Entry point for index.html
+        privacy: './src/privacy.html', // Entry point for privacy.html
+      },
+    },
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  publicDir: '../public',
+});
