6672e394095934ace2e294493e845cab1eee8497
Surya Sankar <suryashankar.m@gmail.com>
2024-12-01 16:31:36 +0530

    Updated links

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import nunjucks from 'vite-plugin-nunjucks';
 
 export default defineConfig({
+  base: '/',
   plugins: [
     nunjucks({
         templatesDir: './',
